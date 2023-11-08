69a6f74ff398e19d000139619cd9e86a730b9dca
Aamir Azad <aamirmazad@gmail.com>
2023-11-08 15:30:15 +0000

    It works

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/user")
+def user():
+    return render_template('user.html')
