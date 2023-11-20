049b55ca609006e3034d6223906d8abcf76ec786
Aamir Azad <aamirmazad@gmail.com>
2023-11-20 14:54:50 +0000

    Move lookup_user to helpers

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 from flask import Flask, render_template, request
-from helpers import lookup_user
+from helpers import lookup_user, get_db, query_db
 import logging
-import sqlite3
-
 
 logging.basicConfig(level=logging.DEBUG)
 
-
 app = Flask(__name__)
-
-def get_db():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def index():
@@ -26,19 +12,24 @@ def index():
 
 @app.route("/user")
 def user():
-    if request.args.get("q") is None:
-        return render_template("user.html")
-    user = request.args.get("q")
-    user_data = lookup_user(user)
-    # Check to make sure that user exists
-    if not user_data:
+    # if request.args.get("q") is None:
+    #     return render_template("user.html")
+    # user = request.args.get("q")
+    # user_data = lookup_user(user)
+    # # Check to make sure that user exists
+    # if not user_data:
+    #     return render_template("error.html")
+    # # Add the user's score to the database
+    # conn = get_db()
+    # # conn.execute('INSERT INTO user (score, user_id) VALUES (?, ?)', (user_data["score"],user_data['id']),)
+    # conn.commit()
+    # conn.close()
+    # database = query_db('SELECT * FROM user WHERE user_id = ?', [user_data["id"]])
+    # database_dict = [dict(row) for row in database]
+    # # Render the username input form
+    # return render_template("user.html", database=database_dict)
+    database_dict = lookup_user(request.args.get("q"))
+
+    if database_dict is None:
         return render_template("error.html")
-    # Add the user's score to the database
-    conn = get_db()
-    # conn.execute('INSERT INTO user (score, user_id) VALUES (?, ?)', (user_data["score"],user_data['id']),)
-    conn.commit()
-    conn.close()
-    database = query_db('SELECT * FROM user WHERE user_id = ?', [user_data["id"]])
-    database_dict = [dict(row) for row in database]
-    # Render the username input form
     return render_template("user.html", database=database_dict)
--- helpers.py
+++ helpers.py
@@ -1,12 +1,25 @@
 import simplejson as json
 import requests
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 def lookup_user(user):
     """Lookup user's states"""
-
+    # Make sure an argument was given
+    if user is None:
+        return None
     # Change username to id, then get data
     try:
-
         if not user.isnumeric():
             url = (
                 f"https://api.foldingathome.org/search/user?query={user}"
@@ -14,16 +27,26 @@ def lookup_user(user):
 
             user_stats = (requests.get(url)).json()
             user_id = user_stats[0]["id"]
-        
         else:
             user_id = user
-
         url = (
             f"https://api.foldingathome.org/uid/{user_id}"
         )
         user_data = requests.get(url).json()
-        return user_data
 
-    except (requests.RequestException, ValueError, KeyError, IndexError):
+    except:
         return None
+    
+    # Check to make sure that user exists
+    if not user_data:
+        return None
+    # Add the user's score to the database
+    conn = get_db()
+    conn.execute('INSERT INTO user (score, user_id) VALUES (?, ?)', (user_data["score"],user_data['id']),)
+    conn.commit()
+    conn.close()
+    database = query_db('SELECT * FROM user WHERE user_id = ?', [user_data["id"]])
+    database_dict = [dict(row) for row in database]
+    # Render the username input form
+    return database_dict
 
