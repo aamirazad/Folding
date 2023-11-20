238ee7926297e09a349864e09f67b5e1f1aa4b75
Aamir Azad <aamirmazad@gmail.com>
2023-11-20 15:25:16 +0000

    Clean some things up

--- app.py
+++ app.py
@@ -12,24 +12,9 @@ def index():
 
 @app.route("/user")
 def user():
-    # if request.args.get("q") is None:
-    #     return render_template("user.html")
-    # user = request.args.get("q")
-    # user_data = lookup_user(user)
-    # # Check to make sure that user exists
-    # if not user_data:
-    #     return render_template("error.html")
-    # # Add the user's score to the database
-    # conn = get_db()
-    # # conn.execute('INSERT INTO user (score, user_id) VALUES (?, ?)', (user_data["score"],user_data['id']),)
-    # conn.commit()
-    # conn.close()
-    # database = query_db('SELECT * FROM user WHERE user_id = ?', [user_data["id"]])
-    # database_dict = [dict(row) for row in database]
-    # # Render the username input form
-    # return render_template("user.html", database=database_dict)
+    if not request.args.get("q"):
+        return render_template("user.html")
     database_dict = lookup_user(request.args.get("q"))
-
     if database_dict is None:
         return render_template("error.html")
     return render_template("user.html", database=database_dict)
