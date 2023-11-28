4913811801c50e46d802703fe02327eae936ae71
Aamir Azad <aamirmazad@gmail.com>
2023-11-28 14:15:25 +0000

    Init scripts folder

---
+++ scripts/bun.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd /workspace/folding && curl -fsSL https://bun.sh/install | bash && source /home/gitpod/.bashrc
---
+++ scripts/flask.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd /workspace/folding && python3 -m venv .venv && . .venv/bin/activate && flask run --debug
