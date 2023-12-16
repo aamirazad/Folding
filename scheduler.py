17fe6690f97e7c19400b7096111f159a27c61c5e
Aamir Azad <13365066-aamirazad@users.noreply.gitlab.com>
2023-12-16 10:43:12 -0500

    Change scheduler to utc(gmt)

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
 from helpers import auto_save, daily_save
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-scheduler = BlockingScheduler()
+scheduler = BlockingScheduler({'apscheduler.timezone': 'UTC'})
 scheduler.add_job(auto_save, 'cron', minute='1')
 scheduler.add_job(daily_save, 'cron', hour='11', minute='59') 
 scheduler.print_jobs()
